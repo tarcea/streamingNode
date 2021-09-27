14469a0a3b5d76f22fb23a1b02176f74f280bce8
tarcea <tarcea@gmail.com>
2021-09-27 22:01:27 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.get('/video', (req, res) => {
+  const range = req.headers.range;
+  const videoPath = './sample.mp4';
+  const videoSize = fs.statSync(videoPath).size;
+  const chunkSize = 1 * 1e+6;
+  const start = range != undefined ? Number(range.replace(/\D/g, "")) : 0;
+  const end = Math.min(start + chunkSize, videoSize - 1);
+
+  const contentLength = end - start + 1;
+
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4"
+  }
+  res.writeHead(206, headers);
+
+  const stream = fs.createReadStream(videoPath, { start, end })
+  stream.pipe(res);
+
+});
+
+app.listen(3000);
